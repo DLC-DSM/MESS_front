950267b76dc1af7a1d08684d0029a10f2abd449b
SeoJiYu <jihoseo2008@gmail.com>
2024-10-18 18:51:52 +0900

    start setting

---
+++ src/style/colors.ts
@@ -0,0 +1,26 @@
+const Colors = {
+    Black: "#000000",
+    Gray900: "#111111",
+    Gray800: "#222222",
+    Gray700: "#444444",
+    Gray600: "#666666",
+    Gray500: "#888888",
+    Gray400: "#AAAAAA",
+    Gray300: "#cccccc",
+    Gray200: "#d9d9d9",
+    Gray100: "#eeeeee",
+    Gray50: "#f6f6f6",
+    White: "#ffffff",
+    Orange900: "#331E00",
+    Orange800: "#522E01",
+    Orange700: "#704405",
+    Orange600: "#AD6A0B",
+    Orange500: "#F08F18",
+    Orange400: "#F0A235",
+    Orange300: "#FAC064",
+    Orange200: "#FAD38C",
+    Orange100: "#FAE7BE",
+    Orange50: "#FAF5EB",
+} as const
+
+export default Colors
